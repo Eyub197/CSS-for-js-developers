6a817b5e3786682da29cb411314126578deaeb1a
Eyub <eyubmehmed122@gmail.com>
2024-12-27 10:52:36 +0200

    Module 3 exercise 8 solution

--- 9-Little-Big-Details/8ex-rising-nav-link/app.jsx
+++ 9-Little-Big-Details/8ex-rising-nav-link/app.jsx
@@ -4,16 +4,46 @@ function NavLink({ href, children }) {
   return (
     <NavLinkAnchor href={href}>
       {children}
+      <Revealed aria-hidden={true}>
+        {children}
+      </Revealed>
     </NavLinkAnchor>
   );
 }
 
 const NavLinkAnchor = styled.a`
+  position: relative;
   text-decoration: none;
   color: inherit;
   font-weight: 500;
   font-size: 1.25rem;
 `;
+
+const Revealed = styled.span`
+  color: hsl(333deg 100% 50%);
+  position: absolute;
+  top: 0;
+  left: 0;
+  filter: drop-shadow(0px 0px 4px white);
+  clip-path: polygon(
+    0% 100%,
+    100% 100%,
+    100% 100%,
+    0% 100%
+  );
+  transition: clip-path 1000ms;
+
+  ${NavLinkAnchor}:hover & {
+    clip-path: polygon(
+      0% 0%,
+      100% 0%,
+      100% 100%,
+      0% 100%
+    );
+    transition: clip-path 300ms;
+  }
+`;
+
 
 const App = () => (
   <header>
